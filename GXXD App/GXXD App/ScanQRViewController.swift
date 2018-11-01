4c5c06a1ffaee3b7b41993c3963a09a2f8e9e8fd
Pedro <Pedroe.ramirez2015@gmail.com>
2018-10-31 20:28:50 -0700

    Fixed error with QRScanner not appearing and placed the QRScanner in the correct ViewController

--- GXXD App/GXXD App/ScanQRViewController.swift
+++ GXXD App/GXXD App/ScanQRViewController.swift
@@ -7,15 +7,53 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ScanQRViewController: UIViewController {
+class ScanQRViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate{
+    
+    var video = AVCaptureVideoPreviewLayer()
     
     @IBAction func ScanQR2Home(_ sender: Any) {
         self.performSegue(withIdentifier: "ScanQR2HomeSegue", sender: self)
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+            //Creating session
+            let session = AVCaptureSession()
+            
+            //Define capture device
+            let captureDevice = AVCaptureDevice.default(for: AVMediaType.video)
+            
+            //Grabbing raw data from camera/capture device
+            do
+            {
+                let input = try AVCaptureDeviceInput(device: captureDevice!)
+                session.addInput(input)
+            }
+            catch
+            {
+                print("Error")
+            }
+            
+            //converting input to output
+            let output = AVCaptureMetadataOutput()
+            session.addOutput(output)
+            
+            output.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            
+            //Reads QR data
+            output.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
+            
+            //return to user video input (what the camera is seeing)
+            video = AVCaptureVideoPreviewLayer(session: session)
+            video.frame = view.layer.bounds
+            view.layer.addSublayer(video)
+            
+            session.startRunning()
+            
     }
     
     
