8826b1c31411038eacac680e895822c6d1b1406f
Pedro <Pedroe.ramirez2015@gmail.com>
2018-11-06 14:11:35 -0800

    Added working buttons and returns to the SideView

---
+++ GXXD App/GXXD App/AboutViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AboutViewController.swift
+//  GXXD App
+//
+//  Created by Pedro Ramirez on 11/6/18.
+//  Copyright © 2018 GXXDPAY. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+}
---
+++ GXXD App/GXXD App/SideBarViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SideViewViewController.swift
+//  GXXD App
+//
+//  Created by Pedro Ramirez on 11/6/18.
+//  Copyright © 2018 GXXDPAY. All rights reserved.
+//
+
+import UIKit
+
+class SideBarViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+}
+
